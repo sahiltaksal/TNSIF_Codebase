0eae1b2ccf17cff109ac61bb3c71104b31b6cbd4
sahiltaksal <sahiltaksal07@gmail.com>
2025-07-04 11:13:03 +0530

    TNSIF DAY 6 USE OF STATIC KEYWORD

---
+++ TNSIF_DAY6/src/static_keyword/Executor.java
@@ -0,0 +1,16 @@
+package static_keyword;
+
+public class Executor {
+
+	public static void main(String[] args) {
+		
+		Myclass.display();
+		
+		System.out.println();
+		
+		Myclass o1=new Myclass();
+		System.out.println(o1);
+
+	}
+
+}
