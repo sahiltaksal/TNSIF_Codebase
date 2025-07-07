b1bbc4cff225a01dcb31b391345b1ed34fb3e9a2
sahiltaksal <sahiltaksal07@gmail.com>
2025-07-07 15:03:13 +0530

    Interface

---
+++ TNSIF_DAY7/src/com/tnsif/abstraction/Shape.java
@@ -0,0 +1,16 @@
+package com.tnsif.abstraction;
+
+public abstract class Shape {
+	
+	protected float area;
+	
+	//abstract method
+	
+	public abstract void calarea();
+	
+	public void show() {
+		System.out.println("Area is : "+area);
+
+	}
+
+}
